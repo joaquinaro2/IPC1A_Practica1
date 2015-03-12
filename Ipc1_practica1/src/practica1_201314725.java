75a11cbc13eaee217e2fdcab338ec8a252fe28ef
Joaquín Haroldo Arévalo Lacayo <joaquinharoldo@hotmail.com>
2015-03-12 14:57:30 -0600

    primer commit

---
+++ Ipc1_practica1/src/practica1_201314725.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+
+public class practica1_201314725 {
+
+	
+	public static void main(String[] args) {
+
+		int numprograma=0;
+		System.out.println("Menú de programas");
+		System.out.println("-Programa 1");
+		System.out.println("-Programa 2");
+		System.out.println("-Programa 3");
+		System.out.println("-Programa 4");
+		System.out.println("-Programa 5");
+		System.out.println("-Programa 6");
+		System.out.println("-Programa 7");
+		System.out.println("-Programa 8");
+		System.out.println("-Programa 9");
+		System.out.println("-Programa 10");
+		System.out.println("-Programa 11");
+		System.out.println("-Programa 12");
+		System.out.println("-Programa 13");
+		System.out.println("-Programa 14");
+		System.out.println("-Programa 15");
+		System.out.println("Ingrese el número del programa que desee ver:");
+		Scanner s = new Scanner (System.in);
+		numprograma=s.nextInt();
+		
+	}
+
+}
