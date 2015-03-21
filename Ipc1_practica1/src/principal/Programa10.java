a85efd86ec93727764a39a2d71491c302b849df8
Joaquín Haroldo Arévalo Lacayo <joaquinharoldo@hotmail.com>
2015-03-20 19:01:26 -0600

    Decimo programa completo

---
+++ Ipc1_practica1/src/principal/Programa10.java
@@ -0,0 +1,27 @@
+package principal;
+
+import java.util.Scanner;
+
+public class Programa10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n;
+		int m;
+		Scanner s= new Scanner (System.in);
+				System.out.println("Piramide Invertica de:");
+		n=s.nextInt();
+		m=n;
+		for (int f=1; f<n+1; f++) { 
+			for (int es=1;es<f; es++)
+				System.out.print(" ");
+            for (int ast=n+1; ast>f; ast--)  
+                System.out.print (m-n+1+" "); 
+             ++m;
+             
+            System.out.println(); 
+    }
+		
+	}
+
+}
