a98a64b22a4c5f0c7486cc48af02016fae6717d9
Joaquín Haroldo Arévalo Lacayo <joaquinharoldo@hotmail.com>
2015-03-22 11:07:35 -0600

    programa 15

---
+++ Ipc1_practica1/src/principal/Programa15.java
@@ -0,0 +1,36 @@
+package principal;
+
+import java.util.Scanner;
+
+public class Programa15 {
+	public static boolean primo (int num){
+		boolean prim=true;
+		int divisor=2;
+		while((prim)&&(divisor!=num)){
+			if(num%divisor==0){
+				prim=false;
+			}else{
+				divisor++;
+		}
+	}
+		return prim;
+}
+	public static void main(String[] args) {
+			Scanner sc=new Scanner(System.in);
+			int numero=0;
+			boolean primo;
+			System.out.println("Ingrese numero para ver primos anteriores:");
+			numero=sc.nextInt();
+			
+			for( int numeros=numero;numero>0 ; numero--  ){
+				primo=primo(numero); 
+				if(primo){
+					System.out.println(numero);
+					
+				}
+				}
+	
+			
+		
+	}
+}
