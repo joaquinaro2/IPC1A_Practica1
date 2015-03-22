115371c18368df4075ff14e66a488c1bd6685c78
Joaquín Haroldo Arévalo Lacayo <joaquinharoldo@hotmail.com>
2015-03-22 11:08:39 -0600

    Practica1
    primera parte

---
+++ Ipc1_practica1/src/principal/Programa9.java
@@ -0,0 +1,31 @@
+package principal;
+import java.util.Scanner;  
+public class Programa9 {
+	
+	
+	    public static void main(String[] args) {  
+	      Scanner s=new Scanner(System.in);  
+	      String Unidad[]={"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};  
+	      String Decena[]={"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};  
+	      String Centena[]={"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};  
+	      String Milenio[]={"", "M", "MM", "MMM"};
+	      System.out.println("Ingrese el número arabico que desea convertir a romano:");  
+	      int N = s.nextInt();  
+	      int u=N%10;  
+	      int d=(N/10)%10;  
+	      int c=N/100;  
+	      int m=(N/1000);
+	      	if(N>=1000){          
+	            System.out.println(Milenio[m]+Centena[c]+Decena[d]+Unidad[u]);  
+	        }else if(N>=100){  
+	                System.out.println(Centena[c]+Decena[d]+Unidad[u]);            
+	        }else if(N>=10){
+	            	System.out.println(Decena[d]+Unidad[u]);
+	       	}else  
+	            	System.out.println(Unidad[u]);
+	                   
+	          }            
+	                  
+	    }  
+	 
+	
---
+++ Ipc1_practica1/src/programa1.java
@@ -0,0 +1,48 @@
+
+public class programa1 {
+
+	
+		public static void main(String[] args) {
+			for (int i=1; i<5; i++) { 
+				for (int k=4;k>i; k--)
+					System.out.print(" ");
+	            for (int j=0; j<i; j++) { 
+	                System.out.print ("*"); 
+	                
+	            } 
+	            System.out.println(); 
+	    }
+			for (int i=4; i>0; i--) { 
+				for (int k=4;k>i; k--)
+					System.out.print(" ");
+	            for (int j=0; j<i; j++) { 
+	                System.out.print ("*"); 
+	                
+	            } 
+	            System.out.println(); 
+	    }
+			
+		/*		char espacio = ' '; 
+				char asterisco = '*'; 
+				for (int i= 1; i<=5; i++){ 
+				for (int espacios = 5 - i; espacios >0; espacios--) 
+				System.out.print(espacio); 
+				for (int lineas = 1; lineas < 2 * i; lineas++) 
+				System.out.print(asterisco); 
+				System.out.println(""); 
+				} 
+				for (int i= 4; i>=1; i--){ 
+				for (int espacios = 5 - i; espacios >0; espacios--) 
+				System.out.print(espacio); 
+				for (int lineas = 1; lineas < 2 * i; lineas++) 
+				System.out.print(asterisco); 
+				System.out.println(""); 
+		   }  
+		}
+		*/
+			}
+		}
+
+	
+
+
